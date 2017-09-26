f23e043c5a4281a9c5b734f2ec3676d18799d608
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-26 10:01:58 -0400

    completed 8kyu kata problem dealing with take function

---
+++ ruby/8-kyu/Take_Enumerable/Solutions.rb
@@ -0,0 +1,11 @@
+# My solution
+
+def take list, n
+  list.take(n)
+end
+
+# Other solutions
+
+def take list, n
+  list[0...n]
+end
---
+++ ruby/8-kyu/Take_Enumerable/Testss.rb
@@ -0,0 +1,4 @@
+describe "take" do
+  list = [0,1,2,3,5,8,13]
+  Test.assert_equals(take(list, 3), [0,1,2], "should return the first 3 items")
+end
