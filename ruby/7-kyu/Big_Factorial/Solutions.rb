0a4f8417712af62a37a539f0a8d2eb6909dd9ece
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-28 14:26:38 -0400

    completed big factorial 7kyu kata

---
+++ ruby/7-kyu/Big_Factorial/Solutions.rb
@@ -0,0 +1,27 @@
+
+# My solution
+def factorial(n)
+  n >= 0 ? ((1..n).inject(:*) || 1) : nil
+end
+
+
+
+# Other solutions
+
+def factorial(n)
+  n < 0 ? nil : (1..n).reduce(:*) || 1
+end
+
+
+def factorial(n)
+  @num = n
+  return nil if @num < 0
+  return 1 if @num == 0
+
+  @num.downto(1).reduce(:*)
+end
+
+
+def factorial(n)
+  (2..n).reduce(1, :*) if n >= 0
+end
