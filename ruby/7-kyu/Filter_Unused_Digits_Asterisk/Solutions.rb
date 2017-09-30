2f2e54316aeb0bc2db98501e84b836e22f12e081
Greg Garville Jr <gjgarvil@gmail.com>
2017-09-29 20:08:42 -0400

    added solutions for incomplete unused digit kata

---
+++ ruby/7-kyu/Filter_Unused_Digits_Asterisk/Solutions.rb
@@ -0,0 +1,18 @@
+# Complex solutions
+
+def unused_digits  (*nums)
+  "0123456789".delete(*nums.to_s)
+end
+
+
+def unused_digits(*args)
+  (('0'..'9').to_a - args.join.chars).join
+end
+
+def unused_digits *x
+  '0123456789'.delete(*x.to_s)
+end
+
+def unused_digits(*digits)
+  "0123456789".tr(digits.join,'')
+end
---
+++ ruby/7-kyu/Filter_Unused_Digits_Asterisk/Tests.rb
@@ -0,0 +1,2 @@
+Test.assert_equals(unused_digits(12, 34, 56, 78), "09")
+Test.assert_equals(unused_digits(2015, 8, 26), "3479")
