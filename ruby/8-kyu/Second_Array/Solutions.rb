52906497aee4f387de75ed9e442de7141d3b311c
Greg Garville Jr <gjgarvil@gmail.com>
2017-10-09 11:00:08 -0400

    completed 8kyu second array kata

--- ruby/8-kyu/Second_Array/Solutions.rb
+++ ruby/8-kyu/Second_Array/Solutions.rb
@@ -7,18 +7,3 @@ class Array
     self[1]
   end
 end
-
-
-# Other solutions
-
-class Array
-  def second
-    self.at(1)
-  end
-end
-
-class Array
-  def second
-    size > 1 ? self[1] : nil
-  end
-end
