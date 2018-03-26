a9b22457ec7c67d351fb529a7dbb66defc2f5cab
Greg Garville Jr <gjgarvil@gmail.com>
2018-03-26 14:32:09 -0400

    added cs format

---
+++ ruby/8-kyu/Codeshare_ML/Int_Layout.rb
@@ -0,0 +1,17 @@
+figures = [-7,0,16,-7]
+return the position or positions of the lowest number or numbers
+
+##This is an array that holds number.
+##I think the min Ruby enumerable will provide the answer.
+## The position - 0, 4 (-7 - 0) (-7 - 4)
+
+## Array
+## Get the position of lowest number
+## Array to store my information
+
+## For loop figures - index ( need index) _metohods
+##String manipulation
+## Return index !
+
+
+figures[0,-1]
