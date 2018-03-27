38588c9f874ce77c9851eb58c5994d51f6f86b83
Greg Garville Jr <gjgarvil@gmail.com>
2018-03-27 09:49:19 -0400

    finished, counting without numbers kata

---
+++ ruby/8-kyu/Return_5_WIthout_Numbers/Solutions.rb
@@ -0,0 +1,15 @@
+#My Solution
+
+def unusual_five()
+"fiver"length
+end
+
+# Other Solutions
+def unusual_five()
+a = ['a','b','c','d','e']
+a.count
+end
+
+def unusual_five
+  ''.ord
+end
---
+++ ruby/8-kyu/Return_5_WIthout_Numbers/Tests.rb
@@ -0,0 +1,5 @@
+describe "Basic Test" do
+  it "should return 5" do
+    Test.assert_equals(unusual_five(), 5)
+  end
+end
