ad16f672beb07f70251d5f14f6af905e5a6a240e
Greg Garville Jr <gjgarvil@gmail.com>
2018-03-28 16:48:05 -0400

    added my/other solutions for 8kyu kata

--- ruby/8-kyu/Wall_Pay_to_Array/Solutions.rb
+++ ruby/8-kyu/Wall_Pay_to_Array/Solutions.rb
@@ -1,4 +1,18 @@
-Test.describe("Basic tests") do
-  Test.assert_equals(who_is_paying("Charlie"), ["Charlie","Ch"])
-  Test.assert_equals(who_is_paying("QWwewrwwrtwrge"), ["QWwewrwwrtwrge","QW"])
+#My solution
+def who_is_paying(name)
+  if name.length > 2
+    [name, name[0..1]]
+  else
+    [name]
+  end
+end
+
+#Other Solutions
+
+def who_is_paying(name)
+[name, name[0..1]].uniq
+end
+
+def who_is_paying
+  (name) name.size>2 ? [name,name.slice(0,2)] : [name]
 end
