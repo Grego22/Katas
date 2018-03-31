d81324a7e6dabf4a545e903f8783f5277f5a0459
Greg Garville Jr <gjgarvil@gmail.com>
2018-03-31 11:01:07 -0400

    finished my solution and added other answers for problem

--- ruby/8-kyu/Who_Ate_the_Cookie/Solutions.rb
+++ ruby/8-kyu/Who_Ate_the_Cookie/Solutions.rb
@@ -1,12 +1,30 @@
 #My working solution
 def cookie(x)
-  case x
-    when x.is_a? String
-      "Zach"
-    when x.is_a? Numeric
-      "Monica"
-    else
-      "Who ate the last cookie? It was the dog!"
+  if x.is_a? String
+    "Who ate the last cookie? It was Zach!"
+  elsif x.is_a? Numeric
+    "Who ate the last cookie? It was Monica!"
+  else
+    "Who ate the last cookie? It was the dog!"
   end
 end
 
+# Other solutions
+
+def cookie(x)
+  'Who ate the last cookie? It was ' << case x
+                                          when String
+                                            'Zach!'
+                                          when Float, Integer
+                                            'Monica!'
+                                          else
+                                            'the dog!'
+                                        end
+end
+
+def cookie(x)
+  case x
+    when String then"Who ate the last cookie? It was Zach!"
+    when Numeric then"Who ate the last cookie? It was Monica!"
+    else"Who ate the last cookie? It was the dog!"end
+end
