be531a909814029d0e1a4bc04d66f4d4ebc62fa1
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-02 13:01:12 -0400

    added solutions for last kata, need to add comments for .each_with_index

---
+++ ruby/8-kyu/Check_the_Exam_comparing_arrays/Solutions.rb
@@ -0,0 +1,22 @@
+# Solutions
+def check_exam(arr1,arr2)
+  score = 0
+  arr1.each_with_index { |value, index|
+    if arr2[index].empty? then
+    elsif value == arr2[index] then
+      score += 4
+    elsif value != arr2[index] then
+      score -= 1
+    end
+  }
+  return 0 if score < 0
+  return score
+end
+
+def check_exam(arr1,arr2)
+  result = 0
+  arr1.each_with_index do |a,i|
+    result += a == arr2[i] ? 4 : arr2[i] == "" ? 0 : -1
+  end
+  result = result > 0 ? result : 0
+end
