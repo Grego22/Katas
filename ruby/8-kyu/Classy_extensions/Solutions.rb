f366d5c3df0537c53e9bf5d22030523d973670fd
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-04 00:37:05 -0400

    added final solution/other working contributed solutions

--- ruby/8-kyu/Classy_extensions/Solutions.rb
+++ ruby/8-kyu/Classy_extensions/Solutions.rb
@@ -3,5 +3,28 @@ class Cat < Animal
   def initialize(name)
     @name = name
   end
+end
 
+#updated solution
+class Cat < Animal
+  def initialize(name)
+    @name = name
+  end
+  def speak
+    "#{@name} meows."
+  end
+end
+
+# other solutions
+class Cat < Animal
+  def speak
+    "#{@name} meows."
+  end
+end
+
+class Cat < Animal
+  attr_reader :name
+  def speak
+    "#{name} meows."
+  end
 end
