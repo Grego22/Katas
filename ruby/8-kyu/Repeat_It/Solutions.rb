88b2e02e96de77642c820274fe9fa5e1590a5e2e
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-14 18:48:42 -0400

    added is_a? in lieu of kind_of? to weed out non strings

--- ruby/8-kyu/Repeat_It/Solutions.rb
+++ ruby/8-kyu/Repeat_It/Solutions.rb
@@ -2,3 +2,37 @@
 def repeat_it(string,n)
   string % 1 == 0 ? "Not a string" : string * n
 end
+
+
+# final solution
+def repeat_it(string,n)
+  string.is_a?(String) ? string * n : "Not a string"
+end
+
+
+# other solutions
+def repeat_it(string,n)
+  if !string.is_a?(String)
+    return "Not a string"
+  else
+    return string*n
+  end
+end
+
+class Object
+  def c(n)
+    "Not a string"
+  end
+end
+class String
+  def c(n)
+    self*n
+  end
+end
+def repeat_it(obj,n)
+  obj.c(n)
+end
+
+def repeat_it(string,n)
+  string.class==String ? string*n : "Not a string"
+end
