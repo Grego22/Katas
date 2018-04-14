fa78656d5444485c062f7693b28aab24659714c5
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-14 18:41:48 -0400

    added repeat_it problem.  Got most tests passing, but issue with instance_of string?

---
+++ ruby/8-kyu/Repeat_It/Solutions.rb
@@ -0,0 +1,4 @@
+# current working solution
+def repeat_it(string,n)
+  string % 1 == 0 ? "Not a string" : string * n
+end
---
+++ ruby/8-kyu/Repeat_It/Tests.rb
@@ -0,0 +1,2 @@
+Test.assert_equals(repeat_it("*", 3), "***")
+Test.assert_equals(repeat_it("Hello", 5), "HelloHelloHelloHelloHello")
