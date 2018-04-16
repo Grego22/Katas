cd1e360a8cdb1dca4968db1662bcb2b7716f1b6c
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-16 00:11:56 -0400

    added method that converts int to string, passing 85% of tests

--- ruby/8-kyu/Century_From_Year/SOlutions.rb
+++ ruby/8-kyu/Century_From_Year/SOlutions.rb
@@ -1,2 +1,14 @@
 #current working solution
-could write a case statement, far form best practice 
+could write a case statement, far form best practice
+end
+#failing 15% of tests
+def century(year)
+  while year
+    x = year.to_s.chars.map(&:to_i)
+    if x[-1] == 0
+      year/100
+    else
+      (year/100) + 1
+    end
+  end
+
