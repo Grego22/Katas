e922b37e5198ed33caad9454b78087aa944dc3e8
Greg Garville Jr <gjgarvil@gmail.com>
2018-04-28 12:34:00 -0400

    updated solution, but still failing a few tests

--- ruby/7-kyu/Simple_Elevator/Solutions.rb
+++ ruby/7-kyu/Simple_Elevator/Solutions.rb
@@ -2,3 +2,8 @@
 def goto(level, button)
   level <= 3 && level > button.to_i ? level-button.to_i : level <= 3 && level < button.to_i ? button.to_i - level : 0
 end
+
+updated working solution
+def goto(level, button)
+  level < 4  button.to_i - level : 0
+end
